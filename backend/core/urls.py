456a4cbf870f1f7275f570d0e5cbad29525e6e19
Wilfried <wilfried.ndr@gmail.com>
2025-09-12 10:49:04 +0200

    changement route portail admin django

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,7 +7,7 @@ def health(_request):
     return JsonResponse({"status": "ok"})
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("apiAdmin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("guides_api.urls")),
